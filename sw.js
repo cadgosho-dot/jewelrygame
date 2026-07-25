86154e58176d5ae27fe71caa6c61d773d68028e9
cadgosho-dot <cadgosho@gmail.com>
2026-07-25 11:50:35 +0900

    337

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '0.10.335';
+const VERSION = '0.10.337';
 const APP_CACHE = `jewelrygame-app-v${VERSION}`;
 const RUNTIME_CACHE = `jewelrygame-runtime-v${VERSION}`;
 const APP_SHELL = [

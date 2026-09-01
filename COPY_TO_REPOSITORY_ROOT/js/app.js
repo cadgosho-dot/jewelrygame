3e0d183834e9ba03d18418cbfa5344d95d257a04
cadgosho-dot <cadgosho@gmail.com>
2026-09-01 12:45:34 +0900

    824

--- COPY_TO_REPOSITORY_ROOT/js/app.js
+++ COPY_TO_REPOSITORY_ROOT/js/app.js
@@ -5,7 +5,7 @@ import {
   clock, nextWeather, AQUARIUM_CONFIG, createInitialAquariumState, normalizeAquariumState,
 } from './game-data.js?v=0.10.823';
 
-const UI_BUILD_VERSION = '0.10.823';
+const UI_BUILD_VERSION = '0.10.824';
 import { configureAudio, unlockAudio, releaseStartupAudioHold, applyAudioSettings, switchAudio, updateMainEnvironment, playSfx, startPoliceSiren, setPoliceSirenGain, stopPoliceSiren, startWristFoundDarkDrone, stopWristFoundDarkDrone, vibrate, suspendAudio, resumeAudio, stopMealAudio, duckCurrentAmbient } from './audio.js?v=0.10.823';
 import { resolveAudioScene } from './audio-scene-map.js?v=0.10.823';
 import { japaneseHolidayName } from './japan-holidays.js';

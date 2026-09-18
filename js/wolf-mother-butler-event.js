c4ea3bcc9bf807ec0475dc270bbbf4213e0b0f80
cadgosho-dot <cadgosho@gmail.com>
2026-09-19 07:19:34 +0900

    0.10.951参照同期 後半

--- js/wolf-mother-butler-event.js
+++ js/wolf-mother-butler-event.js
@@ -1,4 +1,4 @@
-import { suspendAudio, resumeAudio, suspendBgm, resumeBgm, playSfx, vibrate } from './audio.js?v=0.10.950';
+import { suspendAudio, resumeAudio, suspendBgm, resumeBgm, playSfx, vibrate } from './audio.js?v=0.10.951';
 
 const OVERLAY_ID = 'jxj-wolf-mother-butler-overlay';
 const STYLE_ID = 'jxj-wolf-mother-butler-style';

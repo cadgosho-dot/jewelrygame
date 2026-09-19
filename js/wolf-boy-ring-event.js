13f0f0186254e4c24c015310a8b33411792085b5
cadgosho-dot <cadgosho@gmail.com>
2026-09-19 11:05:30 +0900

    v0.10.951 とんかつイベントコードを最新mainへ移植

--- js/wolf-boy-ring-event.js
+++ js/wolf-boy-ring-event.js
@@ -1,4 +1,4 @@
-import { suspendAudio, resumeAudio, playSfx, vibrate } from './audio.js?v=0.10.950';
+import { suspendAudio, resumeAudio, playSfx, vibrate } from './audio.js?v=0.10.951';
 
 const OVERLAY_ID = 'jxj-wolf-boy-ring-overlay';
 const STYLE_ID = 'jxj-wolf-boy-ring-style';

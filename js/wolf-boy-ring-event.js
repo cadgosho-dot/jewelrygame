72445573c1de673f4751361d0a90a7646595bb30
cadgosho-dot <cadgosho@gmail.com>
2026-09-20 08:25:16 +0900

    v0.10.954 バージョン同期と監査修正

--- js/wolf-boy-ring-event.js
+++ js/wolf-boy-ring-event.js
@@ -1,4 +1,4 @@
-import { suspendAudio, resumeAudio, playSfx, vibrate } from './audio.js?v=0.10.952';
+import { suspendAudio, resumeAudio, playSfx, vibrate } from './audio.js?v=0.10.954';
 
 const OVERLAY_ID = 'jxj-wolf-boy-ring-overlay';
 const STYLE_ID = 'jxj-wolf-boy-ring-style';

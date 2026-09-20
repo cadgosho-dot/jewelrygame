8afcdc03a4b59d666092b938944fccd90eaffa7b
cadgosho-dot <cadgosho@gmail.com>
2026-09-20 13:06:02 +0900

    v0.10.955: js/finance/home-property-controller.js のビルド参照を同期

--- js/finance/home-property-controller.js
+++ js/finance/home-property-controller.js
@@ -5,7 +5,7 @@ import {
   homePropertyMonthlyRent,
   homePropertyMoveTotal,
   homePropertyBackgroundAsset,
-} from './home-property.js?v=0.10.954';
+} from './home-property.js?v=0.10.955';
 
 const HOME_PROPERTY_MENU_STYLE_ID = 'home-property-menu-spacing-style';
 

d4fa02f567412a99f09d4610ccda48cdfb4f36e1
cadgosho-dot <cadgosho@gmail.com>
2026-09-21 06:18:38 +0900

    VERSION 0.10.957 同期: js/event-bootstrap.js

--- js/event-bootstrap.js
+++ js/event-bootstrap.js
@@ -1,4 +1,4 @@
 // 独立イベントの起動口。既存の通常イベントUI／クイズイベントUI本体は変更しない。
-import './wolf-boy-ring-event.js?v=0.10.956';
-import './wolf-mother-butler-event.js?v=0.10.956';
-import './events/oyatsu-malatang-event.js?v=0.10.956';
+import './wolf-boy-ring-event.js?v=0.10.957';
+import './wolf-mother-butler-event.js?v=0.10.957';
+import './events/oyatsu-malatang-event.js?v=0.10.957';

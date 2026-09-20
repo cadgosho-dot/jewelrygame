065ac0ae47a976e91a2aa88138a385844d8cdcb6
cadgosho-dot <cadgosho@gmail.com>
2026-09-21 05:41:28 +0900

    v0.10.956 キャッシュ参照を同期

--- js/finance/home-property-controller.js
+++ js/finance/home-property-controller.js
@@ -5,7 +5,7 @@ import {
   homePropertyMonthlyRent,
   homePropertyMoveTotal,
   homePropertyBackgroundAsset,
-} from './home-property.js?v=0.10.955';
+} from './home-property.js?v=0.10.956';
 
 const HOME_PROPERTY_MENU_STYLE_ID = 'home-property-menu-spacing-style';
 
